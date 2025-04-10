55688d3cf9329fe9e546c2381aec6ccb6cd01935
AadityaAgrawal <adityaagrawal554@gmail.com>
2025-04-10 23:34:29 +0530

    Update header.jsx

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,7 +1,7 @@
 const HeaderSection = ()=>{
     return (
         <div >
-            <h1 className="font-bold text-center text-3xl p-3 font-serif">SMART PLANNER TODO APP</h1>
+            <h1 className="font-bold text-center text-3xl p-3 font-serif">TODO APP</h1>
             <p className="font-semibold text-center text-gray-400 pb-8 font-serif ">Your Task Prioritization Partner</p>
         </div>
     )
